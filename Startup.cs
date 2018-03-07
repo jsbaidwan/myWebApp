6f57bbf59c30711f2e457067a38c309343101487
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-03-07 22:55:58 +0530

    this class can be used anywhere

--- Startup.cs
+++ Startup.cs
@@ -17,7 +17,7 @@ namespace myWebApp
 
         public Startup(IConfiguration config)
         {
-
+            Configuration = config;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
